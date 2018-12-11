74531b932f53a9364d2d1d6a731e3f7979e1a616
Zoe Chitty <zlchitty@uchicago.edu>
2018-12-11 15:47:09 -0600

    fix(api-flag): Fix indentation...

--- gen3-client/g3cmd/configure.go
+++ gen3-client/g3cmd/configure.go
@@ -16,7 +16,7 @@ func init() {
 		Long: `Configuration file located at ~/.gen3/config
 	If a field is left empty, the existing value (if it exists) will remain unchanged
 	If no profile is specified, "default" profile is used`,
-	Example: `./gen3-client configure --profile=user1 --cred creds.json --apiendpoint https://data.mycommons.org`,
+		Example: `./gen3-client configure --profile=user1 --cred creds.json --apiendpoint https://data.mycommons.org`,
 		Run: func(cmd *cobra.Command, args []string) {
 
 			cred := conf.ReadCredentials(credFile)
