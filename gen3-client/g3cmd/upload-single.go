9d0db52bd2942825422acae8a3b1d4f9d7d64a8b
Mingfei Shao <mshao1@uchicago.edu>
2018-12-21 12:21:46 -0600

    rename `upload-old` to `upload-single`

--- gen3-client/g3cmd/upload-single.go
+++ gen3-client/g3cmd/upload-single.go
@@ -32,10 +32,10 @@ func init() {
 	var filePath string
 
 	var uploadCmd = &cobra.Command{
-		Use:        "upload-old",
-		Short:      "Upload a file to a GUID",
+		Use:        "upload-single",
+		Short:      "Upload a single file to a GUID",
 		Long:       `Gets a presigned URL for which to upload a file associated with a GUID and then uploads the specified file.`,
-		Example:    `./gen3-client upload-old --profile=<profile-name> --guid=f6923cf3-xxxx-xxxx-xxxx-14ab3f84f9d6 --file=<path-to-file>,`,
+		Example:    `./gen3-client upload-single --profile=<profile-name> --guid=f6923cf3-xxxx-xxxx-xxxx-14ab3f84f9d6 --file=<path-to-file>,`,
 		Deprecated: `use "./gen3-client upload" instead.`,
 		Run: func(cmd *cobra.Command, args []string) {
 			filePaths, err := commonUtils.ParseFilePaths(filePath)
