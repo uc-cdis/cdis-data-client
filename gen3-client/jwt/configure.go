8a70453a48b8236798fa4bbb35bc1e7a40d98671
Mingfei Shao <mshao1@uchicago.edu>
2019-01-14 14:21:47 -0600

    url fix

--- gen3-client/jwt/configure.go
+++ gen3-client/jwt/configure.go
@@ -143,6 +143,11 @@ func (conf *Configure) UpdateConfigFile(cred Credential, configContent []byte, a
 			profile: profile name
 
 	*/
+	apiEndpoint = strings.TrimSpace(apiEndpoint)
+	if apiEndpoint[len(apiEndpoint)-1:] == "/" {
+		apiEndpoint = apiEndpoint[:len(apiEndpoint)-1]
+	}
+
 	lines, found := conf.ReadLines(cred, configContent, apiEndpoint, profile)
 	if found {
 		f, err := os.OpenFile(configPath, os.O_WRONLY|os.O_TRUNC, 0777)
@@ -167,11 +172,6 @@ func (conf *Configure) UpdateConfigFile(cred Credential, configContent []byte, a
 				panic(err)
 			}
 		}()
-
-		apiEndpoint = strings.TrimSpace(apiEndpoint)
-		if apiEndpoint[len(apiEndpoint)-1:] == "\\" {
-			apiEndpoint = apiEndpoint[:len(apiEndpoint)-1]
-		}
 
 		_, err = f.WriteString("[" + profile + "]\n" +
 			"key_id=" + cred.KeyId + "\n" +
