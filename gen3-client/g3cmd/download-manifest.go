dd93c5999f1f618728ff89f38076bade52586ce0
Mingfei Shao <mshao1@uchicago.edu>
2019-06-20 22:55:00 -0500

    feat/download files renaming

--- gen3-client/g3cmd/download-manifest.go
+++ gen3-client/g3cmd/download-manifest.go
@@ -17,7 +17,64 @@ import (
 	"github.com/uc-cdis/gen3-client/gen3-client/jwt"
 )
 
-func downloadFile(numParallel int, reqs []*grab.Request) {
+func lastString(ss []string) string {
+	return ss[len(ss)-1]
+}
+
+func processS3URLForFilename(presignedURL string, guid string, filenameFormat string) string {
+	if filenameFormat != "guid" {
+		urlWithFilename := strings.Split(presignedURL, "?")[0]
+		actualFilename := lastString(strings.Split(urlWithFilename, guid+"/"))
+		if actualFilename != "" {
+			if filenameFormat == "original" {
+				return actualFilename
+			} else if filenameFormat == "combined" {
+				return guid + "." + actualFilename
+			}
+		}
+	}
+	return guid
+}
+
+func downloadFile(guids []string, downloadPath string, filenameFormat string, protocol string, numParallel int) {
+	request := new(jwt.Request)
+	configure := new(jwt.Configure)
+	function := new(jwt.Functions)
+
+	function.Config = configure
+	function.Request = request
+
+	protocolText := ""
+	if protocol != "" {
+		protocolText = "?protocol=" + protocol
+	}
+
+	downloadPath = commonUtils.ParseRootPath(downloadPath)
+	if !strings.HasSuffix(downloadPath, "/") {
+		downloadPath += "/"
+	}
+
+	reqs := make([]*grab.Request, 0)
+	for _, guid := range guids {
+		endPointPostfix := "/user/data/download/" + guid + protocolText
+		msg, err := function.DoRequestWithSignedHeader(profile, "", endPointPostfix, "", nil)
+
+		if err != nil {
+			log.Printf("Download error: %s\n", err)
+		} else if msg.URL == "" {
+			log.Printf("Error in getting download URL for object %s\n", guid)
+		} else {
+			filename := guid
+			if strings.Contains(msg.URL, "X-Amz-Signature") {
+				filename = processS3URLForFilename(msg.URL, guid, filenameFormat)
+			}
+			req, _ := grab.NewRequest(downloadPath+filename, msg.URL)
+			if strings.Contains(msg.URL, "X-Amz-Signature") {
+				req.NoResume = true
+			}
+			reqs = append(reqs, req)
+		}
+	}
 
 	client := grab.NewClient()
 	respch := client.DoBatch(numParallel, reqs...)
@@ -80,27 +137,9 @@ func init() {
 		Long:    `Gets a presigned URL for a file from a GUID and then downloads the specified file.`,
 		Example: `./gen3-client download-manifest --profile=<profile-name> --manifest=<path-to-manifest/manifest.json> --download-path=<path-to-file-dir/>`,
 		Run: func(cmd *cobra.Command, args []string) {
-
-			request := new(jwt.Request)
-			configure := new(jwt.Configure)
-			function := new(jwt.Functions)
-
-			function.Config = configure
-			function.Request = request
-
 			filenameFormat = strings.ToLower(strings.TrimSpace(filenameFormat))
 			if filenameFormat != "original" && filenameFormat != "guid" && filenameFormat != "combined" {
 				log.Fatalln("Invalid option found! Option \"filename-format\" can either be \"original\", \"guid\" or \"combined\" only")
-			}
-
-			protocolText := ""
-			if protocol != "" {
-				protocolText = "?protocol=" + protocol
-			}
-
-			downloadPath = commonUtils.ParseRootPath(downloadPath)
-			if !strings.HasSuffix(downloadPath, "/") {
-				downloadPath += "/"
 			}
 
 			var objects []ManifestObject
@@ -110,35 +149,22 @@ func init() {
 			}
 			json.Unmarshal(manifestBytes, &objects)
 
-			reqs := make([]*grab.Request, 0)
+			guids := make([]string, 0)
 			for _, object := range objects {
 				if object.ObjectID != "" {
-					endPointPostfix := "/user/data/download/" + object.ObjectID + protocolText
-					msg, err := function.DoRequestWithSignedHeader(profile, "", endPointPostfix, "", nil)
-
-					if err != nil {
-						log.Printf("Download error: %s\n", err)
-					} else if msg.URL == "" {
-						log.Printf("Error in getting download URL for object %s\n", object.ObjectID)
-					} else {
-						req, _ := grab.NewRequest(downloadPath, msg.URL)
-						if strings.Contains(msg.URL, "X-Amz-Signature") {
-							req.NoResume = true
-						}
-						reqs = append(reqs, req)
-					}
+					guids = append(guids, object.ObjectID)
 				} else {
 					log.Println("Download error: empty object_id (GUID)")
 				}
 			}
-			downloadFile(numParallel, reqs)
+			downloadFile(guids, downloadPath, filenameFormat, protocol, numParallel)
 		},
 	}
 
 	downloadManifestCmd.Flags().StringVar(&manifest, "manifest", "", "The manifest file to read from")
 	downloadManifestCmd.MarkFlagRequired("manifest")
 	downloadManifestCmd.Flags().StringVar(&downloadPath, "download-path", ".", "The directory in which to store the downloaded files")
-	downloadManifestCmd.Flags().StringVar(&filenameFormat, "filename-format", "original", "format of filename to be used, including \"original\", \"guid\" and \"combined\"")
+	downloadManifestCmd.Flags().StringVar(&filenameFormat, "filename-format", "original", "format of filename to be used, including \"guid\", \"original\" and \"combined\"")
 	downloadManifestCmd.Flags().StringVar(&protocol, "protocol", "", "Specify the preferred protocol with --protocol=s3")
 	downloadManifestCmd.Flags().IntVar(&numParallel, "numparallel", 1, "Number of downloads to run in parallel")
 	RootCmd.AddCommand(downloadManifestCmd)
--- gen3-client/g3cmd/download.go
+++ gen3-client/g3cmd/download.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"strings"
 
-	"github.com/cavaliercoder/grab"
 	"github.com/spf13/cobra"
 	"github.com/uc-cdis/gen3-client/gen3-client/jwt"
 )
@@ -19,7 +18,7 @@ func init() {
 		Use:     "download",
 		Short:   "Download a file from a GUID",
 		Long:    `Gets a presigned URL for a file from a GUID and then downloads the specified file.`,
-		Example: `./gen3-client download --profile=<profile-name> --guid=206dfaa6-bcf1-4bc9-b2d0-77179f0f48fc --file=~/Documents/file_to_download.json`,
+		Example: `./gen3-client download --profile=<profile-name> --guid=206dfaa6-bcf1-4bc9-b2d0-77179f0f48fc`,
 		Run: func(cmd *cobra.Command, args []string) {
 
 			request := new(jwt.Request)
@@ -34,36 +33,16 @@ func init() {
 				log.Fatalln("Invalid option found! Option \"filename-format\" can either be \"original\", \"guid\" or \"combined\" only")
 			}
 
-			protocolText := ""
-			if protocol != "" {
-				protocolText = "?protocol=" + protocol
-			}
-
-			endPointPostfix := "/user/data/download/" + guid + protocolText
-
-			msg, err := function.DoRequestWithSignedHeader(profile, "", endPointPostfix, "", nil)
-
-			if err != nil {
-				log.Printf("Download error: %s\n", err)
-			} else if msg.URL == "" {
-				log.Printf("Error in getting download URL for object %s\n", guid)
-			} else {
-				reqs := make([]*grab.Request, 0)
-				req, _ := grab.NewRequest(downloadPath, msg.URL)
-
-				if strings.Contains(msg.URL, "X-Amz-Signature") {
-					req.NoResume = true
-				}
-				reqs = append(reqs, req)
-				downloadFile(1, reqs)
-			}
+			guids := make([]string, 0)
+			guids = append(guids, guid)
+			downloadFile(guids, downloadPath, filenameFormat, protocol, 1)
 		},
 	}
 
 	downloadCmd.Flags().StringVar(&guid, "guid", "", "Specify the guid for the data you would like to work with")
 	downloadCmd.MarkFlagRequired("guid")
 	downloadCmd.Flags().StringVar(&downloadPath, "download-path", ".", "The directory in which to store the downloaded files")
-	downloadCmd.Flags().StringVar(&filenameFormat, "filename-format", "original", "format of filename to be used, including \"original\", \"guid\" and \"combined\"")
+	downloadCmd.Flags().StringVar(&filenameFormat, "filename-format", "combined", "format of filename to be used, including \"guid\", \"original\" and \"combined\"")
 	downloadCmd.Flags().StringVar(&protocol, "protocol", "", "Specify the preferred protocol with --protocol=gs")
 	RootCmd.AddCommand(downloadCmd)
 }
