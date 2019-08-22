0c67b59ebe5d2acc4dc6cf9cc6c5fff5ce21ea21
Mingfei Shao <mshao1@uchicago.edu>
2019-08-22 16:54:36 -0500

    featr/simplifies download-single

--- gen3-client/g3cmd/download-single.go
+++ gen3-client/g3cmd/download-single.go
@@ -1,11 +1,7 @@
 package g3cmd
 
 import (
-	"strings"
-
 	"github.com/spf13/cobra"
-	"github.com/uc-cdis/gen3-client/gen3-client/commonUtils"
-	"github.com/uc-cdis/gen3-client/gen3-client/jwt"
 )
 
 func init() {
@@ -13,8 +9,9 @@ func init() {
 	var downloadPath string
 	var protocol string
 	var filenameFormat string
-	var overwrite bool
+	var rename bool
 	var noPrompt bool
+	var skipCompleted bool
 
 	var downloadSingleCmd = &cobra.Command{
 		Use:     "download-single",
@@ -22,24 +19,9 @@ func init() {
 		Long:    `Gets a presigned URL for a file from a GUID and then downloads the specified file.`,
 		Example: `./gen3-client download-single --profile=<profile-name> --guid=206dfaa6-bcf1-4bc9-b2d0-77179f0f48fc`,
 		Run: func(cmd *cobra.Command, args []string) {
-
-			request := new(jwt.Request)
-			configure := new(jwt.Configure)
-			function := new(jwt.Functions)
-
-			function.Config = configure
-			function.Request = request
-
-			downloadPath = commonUtils.ParseRootPath(downloadPath)
-			if !strings.HasSuffix(downloadPath, "/") {
-				downloadPath += "/"
-			}
-			filenameFormat = strings.ToLower(strings.TrimSpace(filenameFormat))
-			validateFilenameFormat(downloadPath, filenameFormat, overwrite, noPrompt)
-
 			guids := make([]string, 0)
 			guids = append(guids, guid)
-			downloadFile(guids, downloadPath, filenameFormat, overwrite, protocol, 1, false)
+			downloadFile(guids, downloadPath, filenameFormat, rename, noPrompt, protocol, 1, skipCompleted)
 		},
 	}
 
@@ -49,8 +31,9 @@ func init() {
 	downloadSingleCmd.MarkFlagRequired("guid")
 	downloadSingleCmd.Flags().StringVar(&downloadPath, "download-path", ".", "The directory in which to store the downloaded files")
 	downloadSingleCmd.Flags().StringVar(&filenameFormat, "filename-format", "original", "The format of filename to be used, including \"original\", \"guid\" and \"combined\"")
-	downloadSingleCmd.Flags().BoolVar(&overwrite, "overwrite", false, "Only useful when \"--filename-format=original\", will overwrite any duplicates in \"download-path\" if set to true, will rename file by appending a counter value to its filename otherwise (default: false)")
-	downloadSingleCmd.Flags().BoolVar(&noPrompt, "no-prompt", false, "If set to true, will not display user prompt message for confirmation (default: false)")
-	downloadSingleCmd.Flags().StringVar(&protocol, "protocol", "", "Specify the preferred protocol with --protocol=gs (default: \"\")")
+	downloadSingleCmd.Flags().BoolVar(&rename, "rename", false, "Only useful when \"--filename-format=original\", will rename file by appending a counter value to its filename if set to true, otherwise the same filename will be used")
+	downloadSingleCmd.Flags().BoolVar(&noPrompt, "no-prompt", false, "If set to true, will not display user prompt message for confirmation")
+	downloadSingleCmd.Flags().StringVar(&protocol, "protocol", "", "Specify the preferred protocol with --protocol=gs")
+	downloadSingleCmd.Flags().BoolVar(&skipCompleted, "skip-completed", false, "If set to true, will check for filename and size before download and skip any files in \"download-path\" that matches both")
 	RootCmd.AddCommand(downloadSingleCmd)
 }
