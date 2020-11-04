ee66c20b4067376ff356148edf59c3ac414a0ade
Mingfei Shao <2475897+mfshao@users.noreply.github.com>
2020-11-04 12:11:59 -0600

    fix: guess file name from indexd instead of fence (#78)
    * fix: guess file name from indexd instead of fence
    
    * remove pr template

--- gen3-client/jwt/utils.go
+++ gen3-client/jwt/utils.go
@@ -15,12 +15,13 @@ type AccessTokenStruct struct {
 }
 
 type JsonMessage struct {
-	URL          string `json:"url"`
-	GUID         string `json:"guid"`
-	UploadID     string `json:"uploadId"`
-	PresignedURL string `json:"presigned_url"`
-	FileName     string `json:"file_name"`
-	Size         int64  `json:"size"`
+	URL          string   `json:"url"`
+	GUID         string   `json:"guid"`
+	UploadID     string   `json:"uploadId"`
+	PresignedURL string   `json:"presigned_url"`
+	FileName     string   `json:"file_name"`
+	URLs         []string `json:"urls"`
+	Size         int64    `json:"size"`
 }
 
 type DoRequest func(*http.Response) *http.Response
