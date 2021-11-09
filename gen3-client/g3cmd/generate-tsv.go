de20986a0726baa05caa24599451199843bdbd6b
Mingfei Shao <mshao1@uchicago.edu>
2021-11-08 23:26:41 -0500

    mark generate-tsv as deprecated

---
+++ gen3-client/g3cmd/generate-tsv.go
@@ -0,0 +1,17 @@
+package g3cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	var generateTSVCmd = &cobra.Command{
+		Use:        "generate-tsv",
+		Short:      "Generate a file upload tsv from a template",
+		Long:       `Fills in a Gen3 data file template with information from a directory of files.`,
+		Deprecated: "please use an older version of gen3-client",
+		Run:        func(cmd *cobra.Command, args []string) {},
+	}
+
+	RootCmd.AddCommand(generateTSVCmd)
+}
