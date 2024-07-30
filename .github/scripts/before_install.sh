c89268233295d73a7b4541c3f2d638c62bc86ff7
jacob50231 <jacob50231@gmail.com>
2024-07-30 10:48:54 -0500

    Remove commented line, rename workflows

--- .github/scripts/before_install.sh
+++ .github/scripts/before_install.sh
@@ -3,7 +3,6 @@ pip install -U pip
 pip install awscli
 aws configure set aws_access_key_id $ACCESS_KEY
 aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
-#ln -s $PWD $GOPATH/gen3-client #ln -s $PWD $GOPATH/src/github.com/uc-cdis/gen3-client
 printf "package g3cmd\n\nconst (" >gen3-client/g3cmd/gitversion.go \
     && COMMIT=`git rev-parse HEAD` && echo "    gitcommit=\"${COMMIT}\"" >>gen3-client/g3cmd/gitversion.go \
     && VERSION=`git describe --always --tags` && echo "    gitversion=\"${VERSION}\"" >>gen3-client/g3cmd/gitversion.go \
